0a26e190a69117a3a7d6709d8392124ab2961eda
czross <czr@uw.edu>
2016-10-26 22:54:23 -0700

    done

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -39,19 +39,45 @@ styled.scatterplot <- plot_ly(data = plot.data, x = x, y = y,
 # add a pipe operator and use the layout function to add a title and set the
 # x and y axis to have zeroline = FALSE 
 
+# load in the data set diamonds 
+data("diamonds")
 
+# Use sample() and nrow() to only take a random sampling of 1000 rows from the
+# diamonds data and pass that into a new variable
 d <- diamonds[sample(nrow(diamonds), 1000), ]
+
+# Now create a new plot passing in your sample of a 1000 rows from the diamonds
+# data, set x, y, and z axis as carat, price, and depth.  
+# Finally add makers that is the titled "Clarity, and adds the Clarity of the 
+# diamonds
 p <- plot_ly(d, x = ~carat, y = ~price, z = ~depth) %>%
   add_markers(text = ~paste("Clarity: ", clarity))
 
-# Create a shareable link to your chart
-# Set up API credentials: https://plot.ly/r/getting-started
-chart_link = plotly_POST(p, filename="scatter3d/text")
-chart_link
 
+#Bonus 
+# For this you will make a graph with two differnet Y axis 
+# crete a variable that will have the information for the second Y axis
+# for this the font should be red, overlaying = "y", on the right side, 
+# and should have a title distinguishing it as the second y axis. 
 
+second.axis <- list(
+  tickfont = list(color = "red"),
+  overlaying = "y",
+  side = "right",
+  title = "second y axis"
+)
 
+# Now call the plot_ly() funcion and use pipe operators to have two add_line
+# functions. The first will have x values from 1:3 and y value of 10*(1:3)
+# The second will have x values from 2:4 and y values from 1:3.  Both shoud
+# be named something different.  Finally have a pipe operator connecting the
+# layout() function and add a title for the graph, set the yaxis2 to the previous
+# variable and have a title for the xaxis  
 
-
-
-
+plot_ly() %>%
+  add_lines(x = ~1:3, y = ~10*(1:3), name = "slope of 10") %>%
+  add_lines(x = ~2:4, y = ~1:3, name = "slope of 1", yaxis = "y2") %>%
+  layout(
+    title = "Double Y Axis", yaxis2 = second.axis,
+    xaxis = list(title="x")
+  )
